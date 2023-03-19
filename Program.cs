c296fada340de468f6c561b5174ca8d30f66c1db
Sided911 <dima.puluchu.05@mail.ru>
2023-03-20 00:16:06 +0200

    zadanie 2

--- Program.cs
+++ Program.cs
@@ -10,17 +10,19 @@ namespace ConsoleApp14
     {
         static void Main(string[] args)
         {
-            //нахождение максимального значения 
+            //нахождение максимального значения  и его индекс
             int[] array = { 8, -1, 4, 16, -5, 3 };
+            int indexMax = 0;
             int max = array[0];
             for (int i=1;i<array.Length;i++)
             {
                 if (array[i]>max)
                 {
                     max = array[i];
+                    indexMax = i;
                 }
             }
-            Console.WriteLine($"max={max}");
+            Console.WriteLine($"max={max}\tindexMax={indexMax}");
             Console.ReadKey();
         }
     }
